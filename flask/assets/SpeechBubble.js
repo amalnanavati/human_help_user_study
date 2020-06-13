0b572d7eb53423d085e3eaadb441da5319eaebd8
Amal Nanavati <amaln@uw.edu>
2020-06-12 22:54:43 -0700

    interaction design of queries works, can be controlled by json, and added a working compass

--- flask/assets/SpeechBubble.js
+++ flask/assets/SpeechBubble.js
@@ -1,23 +1,22 @@
 class SpeechBubble {
-  constructor(scene, width, x, y, text) {
+  constructor(scene, width, x, y, text, buttonsData) {
     // x, y is at the bottom of the arrow
+    // buttonsData is a list of objects with "text" and "callbackFunction" attributes.
     this._scene = scene;
     this._width = width;
     this._x = y;
     this._y = y;
     this._text = text;
+    this._buttonsData = buttonsData;
     this.createSpeechBubble();
     this._visibility = true;
-  }
-  setVisibile(visible) {
-    this._bubble.visible = visible;
-    this._speech.visible = visible;
   }
   getVisibile() {
     return this._bubble.visible;
   }
   getBubbleHeight() {
-    return this._speech.getBounds().height * 12 / 10;
+    var padding = this.getBubblePadding();
+    return padding + this._speech.getBounds().height + padding + (this._buttons.length > 0 ? this._buttons[0].height + padding : 0);
   }
   getArrowHeight(bubbleHeight) {
     return bubbleHeight / 4;
@@ -33,10 +32,22 @@ class SpeechBubble {
     return this._width / 7;
   }
   getBubblePadding() {
-    return this._width/10;
+    return this._width/20;
   }
   getSpeechWidth() {
     return this._width - (this.getBubblePadding() * 2);
+  }
+  setVisible(visible) {
+    this._bubble.visible = visible;
+    this._speech.visible = visible;
+    for (var button of this._buttons) {
+      button.visible = visible;
+      if (visible) {
+        button.setInteractive();
+      } else {
+        button.removeInteractive();
+      }
+    }
   }
   setWidth(width) {
     this._width = width;
@@ -55,9 +66,19 @@ class SpeechBubble {
     this._bubble.x = this._x - this.getArrowXOffset();
 
     // The speech position will have to change to remain centered
+    var bubblePadding = this.getBubblePadding();
     var b = this._speech.getBounds();
     this._speech.x = this._bubble.x + (this._width / 2) - (b.width / 2); // the horizontal centering has changed
-    this._speech.y = this._bubble.y + (this.getBubbleHeight() / 2) - (b.height / 2);
+    this._speech.y = this._bubble.y + bubblePadding;
+
+    // The button position has also changed
+    var xOffset = 0;
+    for (i = 0; i < this._buttons.length; i++) {
+      var button = this._buttons[i];
+      button.x = this._speech.x + this._speech.width/2 - this.totalButtonWidth / 2 + xOffset;
+      button.y = this._speech.y + this._speech.height + bubblePadding;
+      xOffset = xOffset + button.width + bubblePadding;
+    }
   }
   setText(text) {
     this._text = text;
@@ -67,36 +88,118 @@ class SpeechBubble {
     this._bubble.scaleY = newHeight / this._originalHeight;
     this._bubble.y = this._y - newHeight;
     var b = this._speech.getBounds();
+    var bubblePadding = this.getBubblePadding();
     this._speech.x = this._bubble.x + (this._width / 2) - (b.width / 2); // the horizontal centering has changed
-    this._speech.y = this._bubble.y + (this.getBubbleHeight() / 2) - (b.height / 2);
+    this._speech.y = this._bubble.y + bubblePadding;
+
+    // The button position has also changed
+    var xOffset = 0;
+    for (i = 0; i < this._buttons.length; i++) {
+      var button = this._buttons[i];
+      button.x = this._speech.x + this._speech.width/2 - this.totalButtonWidth / 2 + xOffset;
+      button.y = this._speech.y + this._speech.height + bubblePadding;
+      xOffset = xOffset + button.width + bubblePadding;
+    }
   }
   setPosition(x, y) {
     // x, y is at the bottom of the arrow
+    var bubblePadding = this.getBubblePadding();
     this._x = x;
     this._y = y;
     var b = this._speech.getBounds();
     this._bubble.x = x - this.getArrowXOffset();
     this._bubble.y = y - this.getHeight();
-    this._speech.setPosition(this._bubble.x + (this._width / 2) - (b.width / 2), this._bubble.y + (this.getBubbleHeight() / 2) - (b.height / 2));
+    this._speech.setPosition(this._bubble.x + (this._width / 2) - (b.width / 2), this._bubble.y + bubblePadding);
+    var xOffset = 0;
+    for (i = 0; i < this._buttons.length; i++) {
+      var button = this._buttons[i];
+      button.x = this._speech.x + this._speech.width/2 - this.totalButtonWidth / 2 + xOffset;
+      button.y = this._speech.y + this._speech.height + bubblePadding;
+      xOffset = xOffset + button.width + bubblePadding;
+    }
+  }
+  setButtons(buttonsData) {
+    var oldButtonsData = this._buttonsData;
+    this._buttonsData = buttonsData;
+
+    var bubblePadding = this.getBubblePadding();
+
+    for (button of this._buttons) {
+      button.destroy();
+    }
+
+    // Create the button(s)
+    this._buttons = [];
+    this.totalButtonWidth = 0.0;
+    for (var buttonData of this._buttonsData) {
+      var button = this._scene.add.text(0, 0, buttonData.text, { fontFamily: 'Arial', fontSize: 20, color: '#000000', align: 'center', backgroundColor: 'rgba(255,0,0,0.5)'});
+      this.totalButtonWidth = this.totalButtonWidth + button.width;
+      button.setInteractive();
+      button.on('pointerdown', buttonData.callbackFunction);
+      button.setDepth(12);
+      this._buttons.push(button);
+    }
+    this.totalButtonWidth = this.totalButtonWidth + bubblePadding*(this._buttons.length - 1);
+
+    var xOffset = 0;
+    for (i = 0; i < this._buttons.length; i++) {
+      var button = this._buttons[i];
+      button.x = this._speech.x + this._speech.width/2 - this.totalButtonWidth / 2 + xOffset;
+      button.y = this._speech.y + this._speech.height + bubblePadding;
+      xOffset = xOffset + button.width + bubblePadding;
+    }
+
+    if ((oldButtonsData.length > 0) ^ (buttonsData.length > 0)) {
+      // Adjust the height for the new text
+      var newHeight = this.getHeight();
+      this._bubble.scaleY = newHeight / this._originalHeight;
+      this._bubble.y = this._y - newHeight;
+      var b = this._speech.getBounds();
+      var bubblePadding = this.getBubblePadding();
+      this._speech.x = this._bubble.x + (this._width / 2) - (b.width / 2); // the horizontal centering has changed
+      this._speech.y = this._bubble.y + bubblePadding;
+    }
   }
   createSpeechBubble ()
   {
+      // Create the bubble speech
       var bubblePadding = this.getBubblePadding();
       this._speech = this._scene.add.text(0, 0, this._text, { fontFamily: 'Arial', fontSize: 20, color: '#000000', align: 'center', wordWrap: { width: this.getSpeechWidth() } });
       this._speech.setDepth(11);
 
+      // Create the button(s)
+      this._buttons = [];
+      this.totalButtonWidth = 0.0;
+      for (var buttonData of this._buttonsData) {
+        var button = this._scene.add.text(0, 0, buttonData.text, { fontFamily: 'Arial', fontSize: 20, color: '#000000', align: 'center', backgroundColor: 'rgba(255,0,0,0.5)'});
+        this.totalButtonWidth = this.totalButtonWidth + button.width;
+        button.setInteractive();
+        button.on('pointerdown', buttonData.callbackFunction);
+        button.setDepth(12);
+        this._buttons.push(button);
+      }
+      this.totalButtonWidth = this.totalButtonWidth + bubblePadding*(this._buttons.length - 1);
+
       var b = this._speech.getBounds();
 
       var bubbleWidth = this._width;//b.width * 12 / 10;//
-      var bubbleHeight = b.height * 12 / 10;//this._height;
+      var bubbleHeight = this.getBubbleHeight();//this._height;
       var arrowHeight = this.getArrowHeight(bubbleHeight);
 
       this._originalWidth = bubbleWidth;
       this._originalHeight = bubbleHeight + arrowHeight;
 
       this._bubble = this._scene.add.graphics({ x: this._x, y: this._y - bubbleHeight - arrowHeight });
+      this._bubble.setDepth(10);
 
-      this._speech.setPosition(this._bubble.x + (bubbleWidth / 2) - (b.width / 2), this._bubble.y + (bubbleHeight / 2) - (b.height / 2));
+      this._speech.setPosition(this._bubble.x + (bubbleWidth / 2) - (b.width / 2), this._bubble.y + bubblePadding);
+      var xOffset = 0;
+      for (i = 0; i < this._buttons.length; i++) {
+        var button = this._buttons[i];
+        button.x = this._speech.x + this._speech.width/2 - this.totalButtonWidth / 2 + xOffset;
+        button.y = this._speech.y + this._speech.height + bubblePadding;
+        xOffset = xOffset + button.width + bubblePadding;
+      }
 
       // //  Bubble shadow
       // this._bubble.fillStyle(0x222222, 0.5);
---
+++ flask/assets/planning.js
@@ -0,0 +1,160 @@
+// Implements the A* algorithm to search for a path from start to one of
+// the goal tiles. It can take in multiple goals (in the form of an array),
+// but only one for the heurstic function.
+function generatePlan(startLoc, endLocs, heuristicEndLoc, isValidTile) {
+  // Used so that each unique (x,y) pair has maximally one object in the
+  // queues/sets.
+  var xyToUniqueObject = {};
+  xyToUniqueObject[String([startLoc.x, startLoc.y])] = {x : startLoc.x, y : startLoc.y, f : distance(heuristicEndLoc, startLoc), dist : 0, visitedFrom : null};
+  for (endLoc of endLocs) {
+    xyToUniqueObject[String([endLoc.x, endLoc.y])] = {x : endLoc.x, y : endLoc.y, f : null, dist : null, visitedFrom : null};
+  }
+
+  // The nodes we have yet to search through
+  var openNodes = new PriorityQueue((a, b) => a.f < b.f);
+  openNodes.push(xyToUniqueObject[String([startLoc.x, startLoc.y])]);
+
+  // The nodes we have already searched through
+  var closedNodes = new Set();
+
+  var goalReachable = false;
+
+  var currNode = null;
+
+  while (!openNodes.isEmpty()) {
+    currNode = openNodes.pop();
+    closedNodes.add(currNode);
+    var reachedGoal = false;
+    for (endLoc of endLocs) {
+      if (currNode.x == endLoc.x && currNode.y == endLoc.y) {
+        goalReachable = true;
+        reachedGoal = true;
+        break;
+      }
+    }
+    if (reachedGoal) {
+      break;
+    }
+
+    for (dNode of [{x:0,y:1},{x:1,y:0},{x:0,y:-1},{x:-1,y:0}]) {
+      var childX = currNode.x + dNode.x;
+      var childY = currNode.y + dNode.y;
+      if (!(String([childX, childY]) in xyToUniqueObject)) {
+        xyToUniqueObject[String([childX, childY])] = {x : childX, y : childY, f : null, dist : null, visitedFrom : null};
+      }
+      var childNode = xyToUniqueObject[String([childX, childY])];
+      // Skip invalid tiles. However, make an exception for the goal in case
+      // another agent is the reason the goal is invalid
+      if (!isValidTile(childNode, true, false)) {
+        var isItAnEndLoc = false;
+        for (endLoc of endLocs) {
+          if (childNode.x == endLoc.x && childNode.y == endLoc.y) {
+            isItAnEndLoc = true;
+            break;
+          }
+        }
+        if (!isItAnEndLoc) {
+          continue;
+        }
+      }
+      if (closedNodes.has(childNode)) {
+        continue;
+      }
+      var heuristicDist = distance(heuristicEndLoc, childNode);
+      var childDist = currNode.dist + 1;
+
+      if (childNode.dist == null || childDist < childNode.dist) {
+        childNode.dist = childDist;
+        childNode.visitedFrom = currNode;
+        childNode.f = childDist + heuristicDist;
+        openNodes.push(childNode);
+      }
+    }
+  }
+
+  if (!goalReachable) {
+    return [];
+  }
+
+  var plan = [];
+  // currNode is already the goal
+  while (currNode.x != startLoc.x || currNode.y != startLoc.y) {
+    plan.unshift({x : currNode.x, y : currNode.y});
+    currNode = currNode.visitedFrom;
+  }
+  // plan.unshift({x : startLoc.x, y : startLoc.y});
+
+  return plan;
+}
+
+// Expands a frontier outwards from startLoc until it finds a point that satisfies
+// goalConstraints. If any child violates pathConstraints while its parent does not,
+// it is not considered. This is a modified version of Djikstra's algorithm.
+function closestPointWithinConstraints(startLoc, goalConstraints, pathConstraints, n_iter) {
+  // Used so that each unique (x,y) pair has maximally one object in the
+  // queues/sets.
+  var xyToUniqueObject = {};
+  xyToUniqueObject[String([startLoc.x, startLoc.y])] = {x : startLoc.x, y : startLoc.y, f : 0};
+
+  // The nodes we have yet to search through
+  var openNodes = new PriorityQueue((a, b) => a.f < b.f);
+  openNodes.push(xyToUniqueObject[String([startLoc.x, startLoc.y])]);
+
+  // The nodes we have already searched through
+  var closedNodes = new Set();
+
+  var goalReachable = false;
+
+  var currNode = null;
+
+  var iter = -1;
+
+  while (!openNodes.isEmpty()) {
+    iter++;
+
+    if (n_iter != null && iter > n_iter) {
+      break;
+    }
+
+    currNode = openNodes.pop();
+    closedNodes.add(currNode);
+    goalReachable = goalConstraints(currNode);
+    if (goalReachable) {
+      break;
+    }
+
+    for (dNode of [{x:0,y:1},{x:1,y:0},{x:0,y:-1},{x:-1,y:0}]) {
+      var childX = currNode.x + dNode.x;
+      var childY = currNode.y + dNode.y;
+      if (!(String([childX, childY]) in xyToUniqueObject)) {
+        xyToUniqueObject[String([childX, childY])] = {x : childX, y : childY, f : null};
+      }
+      var childNode = xyToUniqueObject[String([childX, childY])];
+      // Skip invalid tiles.
+      if (pathConstraints(currNode) && !pathConstraints(childNode)) {
+        continue;
+      }
+      if (closedNodes.has(childNode)) {
+        continue;
+      }
+      var childDist = currNode.f + 1;
+
+      if (childNode.dist == null || childDist < childNode.dist) {
+        childNode.f = childDist;
+        openNodes.push(childNode);
+      }
+    }
+  }
+
+  if (goalReachable) {
+    console.log("iter", iter);
+    return {x : currNode.x, y : currNode.y};
+  } else {
+    return null;
+  }
+
+}
+
+function distance(tile0, tile1) {
+  return Math.abs(tile0.x - tile1.x) + Math.abs(tile0.y - tile1.y);
+}
