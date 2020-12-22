5cbb59296262913610328e3cde572d32f80af3b9
Mridula Venkatesan <mridulavenkatesan@gmail.com>
2020-12-22 14:20:06 -0800

    A* algorithm
    Signed-off-by: Mridula Venkatesan <mridulavenkatesan@gmail.com>

--- simpleServerClientGame/pathPlanning.py
+++ simpleServerClientGame/pathPlanning.py
@@ -27,7 +27,66 @@ def shortestPath(map, start, goal):
       tuple.
     - if goal is not reachable from start, return []
     """
-    return [start]
+    frontier = PriorityQueue()
+    frontier.put(start, False)
+    # open_list = []
+    # closed_list = []
+    # open_list.insert(start, 0)
+
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    current = None
+
+    # check if start is a valid position using same three checks, if not valid, return empty list
+    # remove all types from file including heuristic function types
+    # if not start[0] < 0 or start[0] >= map.shape[0]: # returns a tuple of nxm and gives us n
+    #     return []
+    # if not start[1] < 0 or start[1] >= map.shape[1]:
+    #     return []
+    # if not map[start[0], start[1]]:
+    #     return []
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        (i,j) = current  #just getting values within current
+        for nexti, nextj in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
+            if nexti < 0 or nexti >= map.shape[0]: # returns a tuple of nxm and gives us n
+                continue
+            if nextj < 0 or nextj >= map.shape[1]:
+                continue
+            if map[nexti, nextj]:
+                continue
+            new_cost = cost_so_far[current] + 1
+            next_val = (nexti, nextj)
+            if next_val not in cost_so_far or new_cost < cost_so_far[next_val]:
+                cost_so_far[next_val] = new_cost
+                priority = new_cost + heuristic(next_val, goal)
+                frontier.put(next_val, priority)
+                came_from[next_val] = current
+
+    #outside of the while loop
+    print("current ", current)
+    print("goal ", goal)
+    if current != goal:
+        return []
+
+    path = [goal]
+    while(current != start):
+        current = came_from[current]
+        path.insert(0, current)
+    return path
+
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
 
 def testShortestPath():
     """
@@ -51,6 +110,7 @@ def testShortestPath():
     assert(len(path) == 0)
     # goal is reachable and there is precisely one shortest path
     path = shortestPath(map, (0,0), (0,9))
+    print(path)
     # path == [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8),(0,9)]
     for i in range(len(path)):
         assert(path[i][0] == 0)
@@ -59,6 +119,7 @@ def testShortestPath():
     # goal is reachable and there are several shortest paths (any one can be
     # returned)
     path = shortestPath(map, (0,0), (9,9))
+    print(path)
     assert(len(path) == 19)
 
     # A map with obstacles and only one path through it
@@ -76,6 +137,7 @@ def testShortestPath():
     ).astype(np.bool)
     # goal is reachable
     path = shortestPath(map, (8,1), (1,8))
+    print("final path: ", path)
     pathAnswer = [
         (8,1),(7,1),(6,1),(5,1),(4,1),(3,1),(2,1),(1,1),(1,2),
         (1,3),(2,3),(3,3),(4,3),(5,3),(6,3),(7,3),(8,3),(8,4),
@@ -86,6 +148,7 @@ def testShortestPath():
     for i in range(len(pathAnswer)):
         assert(path[i][0] == pathAnswer[i][0])
         assert(path[i][1] == pathAnswer[i][1])
+
     # goal is a wall
     path = shortestPath(map, (8,1), (9,2))
     assert(len(path) == 0)
